b50c8595f69f920853e0127713aa889647156fb1
DeYoung <mdeyoung@labs.neit.edu>
2019-02-04 11:31:35 -0500

    Updating Repository

--- week3/Simple Form Validation/week2.js
+++ week3/Simple Form Validation/week2.js
@@ -2,10 +2,28 @@
 
 window.addEventListener("load", function(e){
 
-//INCORRECT
-var FirstNameInput = document.querySelector('#input id');
+var inputs = document.querySelectorAll('input[type="text"]');
+var inputError = document.querySelectorAll('span');
+var inputLabel = document.querySelectorAll('label');
 
-this.console.log(FirstNameInput);
+var submitInput = document.querySelector('input[value="submit"]');
 
-
-})
+for (let i = 0; i < inputs.length; i++) {
+inputs[i].addEventListener('blur', function(e){
+    e.preventDefault(); 
+       console.log(inputError[i]);
+    if( this.value === "")
+    { 
+        inputError[i].innerHTML = "*";
+        inputError[i].setAttribute("class","error");
+        inputLabel[i].setAttribute("class", "error");
+    }
+    else
+    {
+        console.log(inputError[i]);
+        inputError[i].innerHTML = "";
+    }
+    
+})//inputs[i].addEventListener CLOSE
+} //for loop CLOSE
+})//window.addEventListener CLOSE
