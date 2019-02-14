426805a74641421c1552f219a62d890c62ed89fa
DeYoung <mdeyoung@labs.neit.edu>
2019-02-14 08:39:00 -0500

    Finished validation

--- week4/week4Lab/regex.js
+++ week4/week4Lab/regex.js
@@ -40,32 +40,60 @@ window.addEventListener("load", function(e){
             inputLabel[i].setAttribute("class", "error");
         }
         }//for loop CLOSE
-    
 
-
-        //REGULAR EXPRESSION TEST
+        //REGULAR EXPRESSION TESTS
+        
+        // FIRST NAME REGEX
         var str = inputs[0].value
-        var patt = /^[a-zA-Z ]+$/;
-        var res = patt.test(str);
+        var patt = /^[a-zA-Z]+$/;
+        var fname_res = patt.test(str);
 
-        document.querySelector("#fn-error").innerHTML = res;
-
-        if (res === false)
+        if (fname_res === false)
         {
-            alert("Your name must not include numbers or special characters");
+            alert("Your first name must not include numbers or special characters");
         }
 
+        //LAST NAME REGEX
         str = inputs[1].value
-        patt = /^[a-zA-Z ]+$/;
-        res = patt.test(str);
+        patt = /^[a-zA-Z]+$/;
+        var lname_res = patt.test(str);
 
-        document.querySelector("#ln-error").innerHTML = res;
-
-        if (res === false)
+        if (lname_res === false)
         {
-            alert("Your name must not include numbers or special characters");
+            alert("Your last name must not include numbers or special characters");
         }
 
+         //EMAIL REGEX
+         str = inputs[2].value
+         patt = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+         var email_res = patt.test(str);
+ 
+         if (email_res === false)
+         {
+             alert("Your email is in the incorrect format");
+         }
+
+         //EMAIL CONFIRMATION REGEX
+         str = inputs[3].value
+         patt = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+         var confirm_res = patt.test(str);
+ 
+         if (confirm_res === false)
+         {
+             alert("Your email confirmation is in the incorrect format");
+         }
+
+         //PHONE REGEX
+         str = inputs[4].value
+         patt = /^[2-9]\d{2}\d{3}\d{4}$/;
+         var phone_res = patt.test(str);
+ 
+        // document.querySelector("#phone-error").innerHTML = res;
+ 
+         if (phone_res === false)
+         {
+             alert("Your phone number is in the incorrect format. Please enter your phone number in the following format: xxxxxxxxxx");
+         }
 
         if (inputs[2].value != inputs[3].value)
         {
@@ -79,12 +107,17 @@ window.addEventListener("load", function(e){
             inputs[2].value != "" &&
             inputs[3].value != "" &&
             inputs[4].value != "" &&
+            fname_res === true &&
+            lname_res === true &&
+            email_res === true &&
+            confirm_res === true &&
+            phone_res === true &&
             inputs[2].value === inputs[3].value
             )
     
         {
-        //document.querySelector('#form').style.display = "none";
-        //document.querySelector('#confirmation').style.display = "block";
+        document.querySelector('#form').style.display = "none";
+        document.querySelector('#confirmation').style.display = "block";
     
         people.push({"#first-name":inputs[0].value, "#last-name":inputs[1].value, "#email":inputs[3].value, "#phone":inputs[4].value});
         console.log(people);
@@ -102,7 +135,6 @@ window.addEventListener("load", function(e){
             }
             output.innerHTML += str; 
         }
-    
     })//submitInput.addEventListener CLOSE}
     
     })//window.addEventListener CLOSE
