24c311d55f6971c163f302175814016b034796d2
DeYoung <mdeyoung@labs.neit.edu>
2019-03-15 13:08:51 -0400

    Animation v1

--- Animation/BallObject.js
+++ Animation/BallObject.js
@@ -1,8 +1,108 @@
-class BallObject
+class GameObject
 {
-    /*constructor(a)
+    constructor(a)
     {
+        this.x = 100;
+        this.y = 100;
+        this.w = 100;
+        this.h = 100;
+        this.vx = 10;
+        this.vy = 0;
+        this.color = "#0095DD";
+        this.angle = 0;
 
     }
-    */
+    drawCircle()
+    {
+        ctx.save();
+            ctx.translate(this.x,this.y);
+            ctx.rotate(this.angle * Math.PI/180);
+            ctx.fillStyle = this.color;
+            ctx.beginPath();
+            ctx.arc(0,0,this.w/2,0, 360 * Math.PI/180, false);
+            ctx.closePath();
+            ctx.fill();
+        ctx.restore();
+    }
+
+    drawRect()
+    {
+        ctx.save();
+            ctx.translate(this.x,this.y);
+            ctx.rotate(this.angle * Math.PI/180);
+            ctx.fillStyle = this.color;
+            ctxfillRect(-this.w/2, -this.h/2, this.w, this.h);
+        ctx.restore();
+    }
+
+    move()
+    {
+        this.x += Math.round(this.vx);
+        this.y += Math.round(this.vy);
+    }
 }
+/*class BallObject
+{
+    constructor(a)
+   /* {
+        this.x = c.width/2;
+        this.y = c.height-30;
+        this.w = 100;
+        this.h = 100;
+        this.vx = 2;
+        this.vy = -2;
+        this.angle = 0;
+        this.color = "#0095DD"
+    }
+
+    drawBall() 
+    {
+        ctx.save();
+            ctx.translate(this.x,this.y);
+            ctx.rotate(this.angle * Math.PI/180);
+            ctx.fillStyle = this.color;
+            ctx.beginPath();
+            ctx.arc(0, 0, this.w/2, 0, 2 * Math.PI, false);
+            ctx.closePath();
+            ctx.fill();
+        ctx.restore();
+    }
+
+    move()
+    {
+        this.x += Matth.round(this.vx);
+        this.y += Math.round(this.vy);
+    }
+
+    
+    {
+        this.x = 100;
+        this.y = 100;
+        this.w = 100;
+        this.h = 100;
+        this.vx = 5;
+        this.vy = -5;
+        this.color = "blue";
+        this.angle = 0;
+
+    }
+    drawCircle()
+    {
+        ctx.save();
+            ctx.translate(this.x,this.y);
+            ctx.rotate(this.angle * Math.PI/180);
+            ctx.fillStyle = this.color;
+            ctx.beginPath();
+            ctx.arc(0,0,this.w/2,0, 2 * Math.PI, false);
+            ctx.closePath();
+            ctx.fill();
+        ctx.restore();
+    }
+
+move()
+{   
+    this.x += Math.round(this.vx);
+    this.y += Math.round(this.vy);
+}
+*/
+
--- Animation/ball.js
+++ Animation/ball.js
@@ -1,8 +1,109 @@
 var c = document.querySelector("canvas");
 var ctx = c.getContext("2d");
 
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.stroke();
+var balls = [];
+var amount = 1;
 
-console.log("Hi");
+var currentState =  "circle";
+
+var states = [];
+
+states["none"] = function(){};
+states["circle"] = function(){
+    for (let i = 0; i < balls.length; i++)
+    {
+        balls[i].drawCircle();
+        balls[i].move();
+        if(balls[i].y > c.height - balls[i].h || balls[i].y < balls[i].w)
+        {
+            balls[i].vy = -balls[i].vy;
+        }
+    }
+};
+
+states["square"] = function(){
+    for (let i = 0; i < balls.length; i++)
+    {
+        balls[i].color = "blue";
+        balls[i].drawCircle();
+        balls[i].move();
+        if(balls[i].y > c.height + balls[i].h/2)
+        {
+            balls[i].y = 0 - balls[i].h/2;
+            currentState = "circle";
+        }
+    }
+};
+
+function rand(low, high)
+{
+    return Math.random() * (high - low) + low;
+}
+
+console.log(rand(-12,8));
+
+for (let i = 0; i < amount; i++)
+{
+    balls[i] = new GameObject();
+    balls[i].x = Math.round(rand(0, c.width));
+    balls[i].y = Math.round(rand(0, c.height)); 
+    balls[i].vy = Math.round(rand(5,15));
+    balls[i].vx = 0;
+
+    balls[i].w = balls[i].vy;; //Math.round(rand(0, c.height));
+    balls[i].h = balls[i].vy;
+
+
+}
+
+main();
+
+function main()
+{
+    ctx.clearRect(0,0,c.width,c.height);
+    
+    states[currentState](); //value of drop down
+    
+    setTimeout(main, 1000/60);
+}
+/*
+var balls = [];
+var amount = 1;
+
+var currentState = "circle";
+
+var states = [];
+
+states["none"] = function(){};
+states["circle"] = function(){
+    for (let i = 0 ; i < balls.length; i++)
+    {
+        balls[i].drawBall();
+        balls[i].move();
+
+        if(balls[i].x + balls[i].vx > canvas.width-balls[i].w/2 || balls[i].x + balls[i].vx < balls[i].w/2) 
+        {
+            balls[i].vx = -balls[i].vx;
+        }
+        if(balls[i].y + balls[i].vy > canvas.height-balls[i].w/2 || balls[i].y + balls[i].vy < balls[i].w/2) 
+        {
+            balls[i].vy = -balls[i].vy;
+        }
+        
+    };
+
+    for (let i = 0; i < amount; i++)
+    {
+        balls[i] = new BallObject();
+    }
+
+main();
+
+function main()
+{
+    ctx.clearRect(0,0,c.width,c.height);
+    
+    states[currentState](); //value of drop down
+    
+    setTimeout(main, 1000/60);
+}*/
